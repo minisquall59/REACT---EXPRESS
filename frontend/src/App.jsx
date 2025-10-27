754c7996957c242da0d3bf4f75ec84101be99dc7
Pierre FOURDIN <fourdin.pfmg@gmail.com>
2025-10-27 11:50:24 +0100

    done exercise 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import './App.css'
+import EmployeeCard from './components/EmployeeCardProps';
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+  // Send the request
+  fetch("https://randomuser.me/api?nat=en")
+    .then((response) => response.json())
+    .then((data) => {
+      setEmployee(data.results[0]);
+    });
+};
+  return (
+    <>
+      <div>
+        <EmployeeCard employee={employee} />
+        <button type="button" onClick={getEmployee}>Get employee</button>
+      </div>
+    </>
+  )
+}
+
+export default App
