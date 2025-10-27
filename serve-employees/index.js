59dd78dbf2b53288aefdf80015caf30ef1c67da0
Pierre FOURDIN <fourdin.pfmg@gmail.com>
2025-10-27 15:43:44 +0100

    Fix: Intégration des fichiers de serve-employees dans le projet

---
+++ serve-employees/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors({ origin: ["http://localhost:5173"] }));
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+    name: {
+    first: "Cecile",
+    last: "PARDO"
+  },
+  email: "C.pardo@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+app.get("/api/employees", (req, res) => {
+  res.json({ results: [sampleEmployee] });
+});
+
+const port = 3310;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
